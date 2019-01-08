ef671503c8cdd4612bbc1c964c511087ef2c58eb
Zeztron <pateltharsh@gmail.com>
2019-01-08 14:27:14 -0500

    set up the event listeners

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
+// =============================================================================
+// QUIZ CONTROLLER
+// =============================================================================
 const quizController = (() => {
-    const private = 10;
-    const privateFn = (a) => {
-        return a + private;
+    
+    // Question Contructor
+    function Question(id, questionText, options, correctAnswer) {
+        this.id = id;
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
     }
+
     return {
-        publicMethod: () => {
-            return privateFn(20);
+        addQuestionOnLocalStorage: (newQuestText, opts) => {
+            console.log("hi");
         }
     }
+    
+
 })();
 
-
-
+// =============================================================================
+// UI CONTROLLER
+// =============================================================================
 const UIController = (() => {
 
+    const domItems = {
+        // Admin panel elements
+        questInsertBtn: document.getElementById("question-insert-btn"),
+        newQuestText: document.getElementById("new-question-text"),
+        adminOptions: document.querySelectorAll(".admin-option")
+    };
+
+    return {
+        getDomItems: domItems
+    };
+
 })();
 
 
-
+// =============================================================================
+// CONTROLLER
+// =============================================================================
 const controller = ((quizCtrl, UICtrl) => {
+    
+    const selectedDomItems = UICtrl.getDomItems;
+
+    selectedDomItems.questInsertBtn.addEventListener("click", () => {
+        // console.log("clicked");
+        quizController.addQuestionOnLocalStorage(selectedDomItems.newQuestText, selectedDomItems.adminOptions);
+    });
 
 })(quizController, UIController);
